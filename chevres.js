970d26bf5223cd59d4141d6b2b6bcf91fedd64c2
Richard Hitier <richard.hitier@co-libri.org>
2014-07-10 23:25:24 +0200

    Use geoCSV to display test csv (overpass shop=farm)

--- chevres.js
+++ chevres.js
@@ -1,5 +1,5 @@
-var z=11;
-var myLL= L.latLng(43.3317,2.5808);
+var z=4;
+var myLL= L.latLng(36.17,-91.10);
 
 
 // create a map in the "map" div, set the view to a given place and zoom
@@ -13,4 +13,38 @@ var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
   attribution: 'OpenStreetMap'
   }).addTo(map);
 
+var chevresLayer = L.geoCsv(null, { firstLineTitles: true, fieldSeparator: ',', });
 
+
+$.ajax ({
+    type:'GET',
+    dataType:'text',
+    url:'datas/chevres.csv',
+   error: function() {
+     alert('Chargement impossible');
+   },
+    success: function(csv) {
+      //var cluster = new L.MarkerClusterGroup();
+        chevresLayer.addData(csv);
+        //cluster.addLayer(bankias);
+        //map.addLayer(cluster);
+        map.addLayer(chevresLayer);
+        map.fitBounds(chevresLayer.getBounds());
+      alert( 'success');
+    },
+   complete: function() {
+      //$('#cargando').delay(500).fadeOut('slow');
+      alert( 'complet');
+   }
+});
+
+
+  var baseLayers = {
+    "OSM": osmLayer
+  };
+
+var overLays = {
+    "chevres": chevresLayer,
+};
+
+L.control.layers(baseLayers, overLays).setPosition('topright').addTo(map);
