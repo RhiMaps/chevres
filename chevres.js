a69f20cbc037a477567d46b617ab49e001cca6a0
Richard Hitier <richard.hitier@co-libri.org>
2014-07-16 14:43:23 +0200

    Change data file to Vincent's one

--- chevres.js
+++ chevres.js
@@ -1,24 +1,29 @@
-var z=4;
-var myLL= L.latLng(43.808,0.887);
+var z = 4;
+var myLL = L.latLng(43.808, 0.887);
 
 // create a map in the "map" div, set the view to a given place and zoom
 var map = L.map('map', {
-  center:myLL,
-  zoom:z});
+    center: myLL,
+    zoom: z
+});
 
 
 // add an OpenStreetMap tile layer
 var osmLayer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-  attribution: 'OpenStreetMap'
-  }).addTo(map);
+    attribution: 'OpenStreetMap'
+}).addTo(map);
 
 var chevresLayer = L.geoCsv(null, {
     firstLineTitles: true,
     fieldSeparator: ',',
-    onEachFeature: function (feature, layer) {
-        var popupContent= '<b>'+feature.properties['name']+'</b>'+
-                          '<img src="./datas/'+feature.properties['imgpath']+'"/>';
-        var popup = L.popup( {minWidth: '400'}).setContent(popupContent);
+    onEachFeature: function(feature, layer) {
+        var popupContent = '<b>' + feature.properties['name'] + '</b>';
+       if ( feature.properties['poids']) popupContent +='</br>'+ feature.properties['poids'] + ' gr.';
+       if ( feature.properties['grasse']) popupContent +='</br>'+ feature.properties['grasse'] + '% mat. grasse';
+                            //+ '<img src="./datas/' + feature.properties['imgpath'] + '"/>';
+        var popup = L.popup({
+            minWidth: '400'
+        }).setContent(popupContent);
         //for (var key in feature.properties) {
         //    var title = chevresLayer.getPropertyTitle(key);
         //    popup += '<b>'+title+'</b><br />'+feature.properties[key]+'<br />';
@@ -26,41 +31,44 @@ var chevresLayer = L.geoCsv(null, {
         layer.bindPopup(popup);
     },
     pointToLayer: function(feature, latlng) {
-          var myIcon = L.icon({
-              iconUrl: 'images/chevre.png',
-              iconSize: [40, 40],
-              iconAnchor: [20, 20],
-              popupAnchor: [0, -20]});
-          var marker = L.marker(latlng, {icon:myIcon} );
-          return  marker;
-      },
+        var myIcon = L.icon({
+            iconUrl: 'images/chevre.png',
+            iconSize: [40, 40],
+            iconAnchor: [20, 20],
+            popupAnchor: [0, -20]
+        });
+        var marker = L.marker(latlng, {
+            icon: myIcon
+        });
+        return marker;
+    },
 });
 
 
-$.ajax ({
-    type:'GET',
-    dataType:'text',
-    url:'datas/chevres.csv',
-   error: function() {
-     alert('Chargement impossible');
-   },
+$.ajax({
+    type: 'GET',
+    dataType: 'text',
+    url: 'datas/chevres_vincent_geocoded.csv',
+    error: function() {
+        alert('Chargement impossible');
+    },
     success: function(csv) {
-      //var cluster = new L.MarkerClusterGroup();
+        //var cluster = new L.MarkerClusterGroup();
         chevresLayer.addData(csv);
         //cluster.addLayer(bankias);
         //map.addLayer(cluster);
         map.addLayer(chevresLayer);
         map.fitBounds(chevresLayer.getBounds());
     },
-   complete: function() {
-      //$('#cargando').delay(500).fadeOut('slow');
-   }
+    complete: function() {
+        //$('#cargando').delay(500).fadeOut('slow');
+    }
 });
 
 
-  var baseLayers = {
+var baseLayers = {
     "OSM": osmLayer
-  };
+};
 
 var overLays = {
     "chevres": chevresLayer,
